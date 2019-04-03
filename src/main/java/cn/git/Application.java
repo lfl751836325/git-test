c8679ee15194f5c08dc09ac599139c1f2439488a
lfl <751836325@qq.com>
2019-04-03 16:32:38 +0800

    tttt

---
+++ src/main/java/cn/git/Application.java
@@ -0,0 +1,12 @@
+package cn.git;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void mian(String[] args){
+        SpringApplication.run(Application.class);
+    }
+}
